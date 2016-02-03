b7074d68296c7215eeddcf34caae903e1d76341d
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-02-03 11:36:32 -0500

    finished UI

--- src/main/webapp/js/demo.js
+++ src/main/webapp/js/demo.js
@@ -25,8 +25,18 @@ function _error(xhr) {
 
 
 $(document).ready(function() {
+  // namespace variables
   var queries = [];
   var query = null;
+
+  // jQuery nodes
+  var $serviceResults = $('.service--results'),
+    $standardResults = $('.standard--results'),
+    $template = $('.result--template'),
+    $output = $('.output'),
+    $query = $('#query');
+
+  $output.hide();
 
   $.get('data/queries.json').then(function(data){
     queries = JSON.parse(data).queries;
@@ -41,13 +51,63 @@ $(document).ready(function() {
 
   $('.input--question-generator').click(function(){
     query = queries[Math.floor(Math.random() * queries.length)];
-    console.log('ask:', query);
-    $('#query').text(query.query);
-    $.get('data/response.json').then(function(data){
-      var responses = JSON.parse(data);
-      console.log(data);
+
+    $query.text(query.query);
+
+    $.get('data/response.json').then(function(data) {
+      var response = JSON.parse(data);
+
+      // standard results - solr
+      $standardResults.empty();
+      response.solr_results.map(createResultNode.bind(null, $template, false))
+      .forEach(function(e){
+        $standardResults.append(e);
+      });
+
+      // service results - solr + ranker
+      $serviceResults.empty();
+      response.ranked_results.map(createResultNode.bind(null, $template, true))
+      .forEach(function(e){
+        $serviceResults.append(e);
+      });
+
+      $output.show();
     }, _error);
 
+    function createResultNode($template, showRanking, result) {
+      var node = $template.last().clone().show();
+
+      node.find('.results--item-text').prepend(result.title);
+      node.find('.results--item-details').text(result.body);
+
+
+      // ranking result
+      if (showRanking){
+        var iconClass = 'results--increase-icon_UP icon-arrow_up';
+        var rank = result.finalRank - result.solrRank;
+        if (rank < 0)
+          iconClass = 'results--increase-icon_DOWN icon-arrow_down';
+
+        node.find('.results--increase-icon').addClass(iconClass);
+        node.find('.results--increase-value').text(Math.abs(rank));
+      } else {
+        node.find('.results--item-rank').remove();
+      }
+
+      node.find('.results--item-score-bar').each(function(i, score) {
+        if ((result.relevance - (i + 1)) >= 0)
+          $(score).addClass('green');
+      });
+
+      var $moreInfo = node.find('.results--more-info');
+      node.find('.results--see-more').click(function() {
+        if ($moreInfo.css('display') === 'none')
+          $moreInfo.fadeIn('slow');
+        else
+          $moreInfo.fadeOut(500);
+      });
+      return node;
+    }
   });
 });
 
